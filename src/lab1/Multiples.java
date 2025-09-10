8e265ecb5d7150e845857ca0f8cd82975f3613a6
Amir Diba <151319977+Amir8-5@users.noreply.github.com>
2025-09-10 12:16:10 -0400

    used set in Multiples.java

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,21 +1,33 @@
 package lab1;
 
+import java.util.HashSet;
+
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples(1000, 3, 5));
+        System.out.println(multiples(16, 3, 5));
     }
 
     public static int multiples(int n, int a, int b) {
+
         int firstMultiple, secondMultiple;
         firstMultiple = secondMultiple = 1;
         int firstCount , secondCount ;
         firstCount = secondCount = 0;
         final int MAX = n;
-        while (a * firstMultiple < MAX && b * secondMultiple < MAX) {
-            firstCount += a * firstMultiple < MAX ? 1 : 0;
-            firstMultiple ++;
-            secondCount += b * secondMultiple < MAX ? 1 : 0;
+        HashSet<Integer> set = new HashSet<>();
+        while (a * firstMultiple < MAX || b * secondMultiple < MAX) {
+            if (a * firstMultiple < MAX && !set.contains(a * firstMultiple)){
+                firstCount++;
+                set.add(a * firstMultiple);
+            }
+
+            if (b * secondMultiple < MAX && !set.contains(b * secondMultiple)){
+                secondCount++;
+                set.add(b * secondMultiple);
+            }
+            firstMultiple++;
             secondMultiple++;
+
         }
         return firstCount + secondCount;
     }
